ed83b15a9881b00416ffc7258b4cbc381bdde8e3
Michal Porteš <michalportes1@gmail.com>
2023-07-09 22:06:33 +0200

    Add search functionality
    Add url, view and template for the search result page.

--- lego/tests.py
+++ lego/tests.py
@@ -53,3 +53,67 @@ class TestGetResponse(TestCase):
         response = self.client.get("/lego/part/999/")
 
         self.assertEqual(response.status_code, 404)
+
+
+class TestSearch(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        LegoSet.objects.create(lego_id="123-1", name="Brick House")
+        LegoPart.objects.create(lego_id="234pr", name="Brick 2 x 4")
+
+    def test_set_found_by_name(self):
+        response = self.client.get("/lego/search/", data={"q": "house"})
+
+        self.assertEqual(response.status_code, 200)
+        self.assertRegex(
+            response.content,
+            b"(?s)Search Results for.*house"
+            b".*123-1.*Brick House",
+        )
+
+    def test_part_found_by_name(self):
+        response = self.client.get("/lego/search/", data={"q": "2 x 4"})
+
+        self.assertEqual(response.status_code, 200)
+        self.assertRegex(
+            response.content,
+            b"(?s)Search Results for.*2 x 4"
+            b".*234pr.*Brick 2 x 4",
+        )
+
+    def test_multiple_results_found_by_name(self):
+        response = self.client.get("/lego/search/", data={"q": "brick"})
+
+        self.assertEqual(response.status_code, 200)
+        self.assertRegex(
+            response.content,
+            b"(?s)Search Results for.*brick"
+            b".*123-1.*Brick House"
+            b".*234pr.*Brick 2 x 4",
+        )
+
+    def test_set_found_by_lego_id(self):
+        response = self.client.get("/lego/search/", data={"q": "123"})
+
+        self.assertEqual(response.status_code, 200)
+        self.assertRegex(
+            response.content,
+            b"(?s)Search Results for.*123"
+            b".*123-1.*Brick House",
+        )
+
+    def test_part_found_by_lego_id(self):
+        response = self.client.get("/lego/search/", data={"q": "234"})
+
+        self.assertEqual(response.status_code, 200)
+        self.assertRegex(
+            response.content,
+            b"(?s)Search Results for.*234"
+            b".*234pr.*Brick 2 x 4",
+        )
+
+    def test_nothing_found(self):
+        response = self.client.get("/lego/search/", data={"q": "999"})
+
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b"Nothing Found", response.content)

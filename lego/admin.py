cd3e4b53c860a3240ed153267d14ae6ae5533df7
Michal Porteš <michalportes1@gmail.com>
2023-08-17 10:44:31 +0200

    Extract the `Shape` model
    This change prepares the schema for the addition of the `LegoPart.color` field. `LegoPart` will consist of two foreign keys: shape and color.

--- lego/admin.py
+++ lego/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import LegoPart, LegoSet, PartInSet
+from .models import Shape, LegoPart, LegoSet, PartInSet
 
+admin.site.register(Shape)
 admin.site.register(LegoPart)
 admin.site.register(LegoSet)
 admin.site.register(PartInSet)

0284d219ebc6ad637d2cd125d3a05a29adf727f3
Michal Porteš <michalportes1@gmail.com>
2023-09-26 09:05:20 +0200

    Add support for different search modes

--- lego/forms.py
+++ lego/forms.py
@@ -3,3 +3,13 @@ from django import forms
 
 class SearchForm(forms.Form):
     q = forms.CharField(max_length=150, label="Search")
+    mode = forms.ChoiceField(
+        choices=(
+            ("all", "everywhere"),
+            ("name", "in names"),
+            ("id", "Lego ID"),
+        ),
+        widget=forms.RadioSelect,
+        initial="all",
+        label="",
+    )

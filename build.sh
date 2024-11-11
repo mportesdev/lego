6afe925bebbc6c1540142d4454503a19e0c52edb
Michal Porteš <michalportes1@gmail.com>
2024-11-11 13:37:39 +0100

    Update build.sh and .gitignore

--- build.sh
+++ build.sh
@@ -6,4 +6,4 @@ pip install -r requirements.txt
 
 python manage.py migrate
 python manage.py collectstatic --no-input
-python manage.py shell -c "$(cat check_superuser.py)"
+python manage.py shell -c 'from django.contrib.auth.models import User; User.objects.get(username="admin")' || python manage.py createsuperuser --no-input --username=admin --email=""
--- check_superuser.py
+++
@@ -1,5 +0,0 @@
-from django.contrib.auth.models import User
-from django.core.management import call_command
-
-if not User.objects.filter(is_superuser=True):
-    call_command("createsuperuser", "--no-input", username="admin", email="")

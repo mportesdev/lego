51261f72f5e062dbf370c08591e3a36a3e37e0c4
Michal Porteš <michalportes1@gmail.com>
2024-12-05 22:33:21 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,3 +7,8 @@ pip install -r requirements.txt
 python manage.py migrate
 python manage.py collectstatic --no-input
 python manage.py shell -c 'from django.contrib.auth.models import User; User.objects.get(username="admin")' || python manage.py createsuperuser --no-input --username=admin --email=""
+
+python manage.py dumpdata lego > lego.json
+python manage.py dumpdata auth.user > user.json
+ln -s lego.log log.txt
+ln -sr lego.json user.json log.txt staticfiles/

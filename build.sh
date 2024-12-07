22afb54c0753848a7440ec3ca583b40fc3016e82
Michal Porteš <michalportes1@gmail.com>
2024-12-07 23:26:36 +0100

    Revert "Store data backup as static text files"
    This reverts commit a26177b026c39b3310e9811ff812b6e570d7339f.

--- build.sh
+++ build.sh
@@ -7,6 +7,3 @@ pip install -r requirements.txt
 python manage.py migrate
 python manage.py collectstatic --no-input
 python manage.py shell -c 'from django.contrib.auth.models import User; User.objects.get(username="admin")' || python manage.py createsuperuser --no-input --username=admin --email=""
-
-python manage.py dumpdata lego > staticfiles/dump.json
-cp --force lego.log staticfiles/

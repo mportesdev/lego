297e60bf27fa8c33c0851df3eb63eeb553b0ef36
Michal Porteš <michalportes1@gmail.com>
2024-12-12 02:01:17 +0100

    Deployment tweaks

--- project/urls.py
+++ project/urls.py
@@ -6,3 +6,9 @@ urlpatterns = [
     path("admin/", admin_site.urls),
     path("lego/", include("lego.urls")),
 ]
+
+from django.shortcuts import redirect
+
+urlpatterns.append(
+    path("", lambda r: redirect("/lego/")),
+)

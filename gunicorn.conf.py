af5177f28b132279622c9890583750de8988f44d
Michal Porteš <michalportes1@gmail.com>
2024-12-12 02:01:17 +0100

    Configure deployment

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -2,6 +2,8 @@ from pathlib import Path
 
 raw_env = (Path(__file__).parent / ".env").read_text().strip().splitlines()
 worker_class = "uvicorn.workers.UvicornWorker"
-reload = True
 accesslog = "-"
 errorlog = "-"
+bind = ["0.0.0.0:8080"]
+user = "app"
+group = "app"

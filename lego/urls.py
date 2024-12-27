ccc291edad978b679f381ab73c1dfa4785c87f06
Michal Porteš <michalportes1@gmail.com>
2024-12-28 00:50:05 +0100

    views: Use generic view for part detail page (#12)

--- lego/urls.py
+++ lego/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
 
-from .views import IndexView, SetDetail, part_detail, search, add_set, login, logout
+from .views import IndexView, SetDetail, PartDetail, search, add_set, login, logout
 
 urlpatterns = [
     path("", IndexView.as_view(), name="index"),
     path("set/add/", add_set, name="add_set"),
     path("set/<lego_id>/", SetDetail.as_view(), name="set_detail"),
-    path("part/<lego_id>/", part_detail, name="part_detail"),
-    path("part/<lego_id>/<int:color_id>/", part_detail, name="part_detail"),
+    path("part/<lego_id>/", PartDetail.as_view(), name="part_detail"),
+    path("part/<lego_id>/<int:color_id>/", PartDetail.as_view(), name="part_detail"),
     path("search/", search, name="search"),
     path("login/", login, name="login"),
     path("logout/", logout, name="logout"),

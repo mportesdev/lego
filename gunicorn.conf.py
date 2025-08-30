bd015601f61aa41640c3175bbac106b54a76d2db
Michal Porteš <michalportes1@gmail.com>
2025-08-30 21:49:33 +0200

    Use `python-dotenv` to load environment variables

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,6 +1,3 @@
-from pathlib import Path
-
-raw_env = (Path(__file__).parent / ".env").read_text().strip().splitlines()
 worker_class = "uvicorn.workers.UvicornWorker"
 reload = True
 accesslog = "-"

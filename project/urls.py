1c11625ccea84c0d1b8e471469e99ff04631efe2
Michal Porteš <michalportes1@gmail.com>
2025-11-25 22:55:05 +0100

    Use django-debug-toolbar in development (#104)

--- project/urls.py
+++ project/urls.py
@@ -6,3 +6,7 @@ urlpatterns = [
     path("admin/", admin_site.urls),
     path("lego/", include("lego.urls")),
 ]
+
+from debug_toolbar.toolbar import debug_toolbar_urls
+
+urlpatterns.extend(debug_toolbar_urls())

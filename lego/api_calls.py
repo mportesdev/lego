dbef764468a5c2ffa05c10e6adf014fabb23145f
Michal Porteš <michalportes1@gmail.com>
2026-01-20 10:14:40 +0100

    Use session and response as context managers

--- lego/api_calls.py
+++ lego/api_calls.py
@@ -39,9 +39,9 @@ def _get_paginated_data(url, session):
 
 def _get_response(url, session=None):
     get_func = session.get if session else requests.get
-    response = get_func(url, auth=auth, headers=headers, timeout=5)
-    response.raise_for_status()
-    return response.json()
+    with get_func(url, auth=auth, headers=headers, timeout=5) as response:
+        response.raise_for_status()
+        return response.json()
 
 
 def _color_name_or_none(color_name):
@@ -51,29 +51,29 @@ def _color_name_or_none(color_name):
 
 
 def get_set_parts(set_lego_id):
-    session = requests.Session()
-    for item in _get_paginated_data(
-        f"{API_URL}/lego/sets/{set_lego_id}/minifigs/", session
-    ):
-        entry = {
-            "lego_id": item["set_num"],
-            "name": item["set_name"],
-            "image_url": item["set_img_url"],
-            "quantity": item["quantity"],
-        }
-        logger.debug(entry)
-        yield entry
+    with requests.Session() as session:
+        for item in _get_paginated_data(
+            f"{API_URL}/lego/sets/{set_lego_id}/minifigs/", session
+        ):
+            entry = {
+                "lego_id": item["set_num"],
+                "name": item["set_name"],
+                "image_url": item["set_img_url"],
+                "quantity": item["quantity"],
+            }
+            logger.debug(entry)
+            yield entry
 
-    for item in _get_paginated_data(
-        f"{API_URL}/lego/sets/{set_lego_id}/parts/", session
-    ):
-        entry = {
-            "lego_id": item["part"]["part_num"],
-            "name": item["part"]["name"],
-            "color_name": _color_name_or_none(item["color"]["name"]),
-            "image_url": item["part"]["part_img_url"],
-            "quantity": item["quantity"],
-            "is_spare": item["is_spare"],
-        }
-        logger.debug(entry)
-        yield entry
+        for item in _get_paginated_data(
+            f"{API_URL}/lego/sets/{set_lego_id}/parts/", session
+        ):
+            entry = {
+                "lego_id": item["part"]["part_num"],
+                "name": item["part"]["name"],
+                "color_name": _color_name_or_none(item["color"]["name"]),
+                "image_url": item["part"]["part_img_url"],
+                "quantity": item["quantity"],
+                "is_spare": item["is_spare"],
+            }
+            logger.debug(entry)
+            yield entry

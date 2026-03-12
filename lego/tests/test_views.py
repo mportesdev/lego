d19cd1b91277a6a2846c6948c5b9e8a7152ae398
Michal Porteš <michalportes1@gmail.com>
2026-03-12 12:01:24 +0100

    tests: Update tests of view functions (#142)
    * Split into classes by view function
    
    * Add a test for authenticated user
    
    * Add tests for POST requests

--- lego/tests/test_views.py
+++ lego/tests/test_views.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.models import AnonymousUser
+from django.contrib.auth.models import AnonymousUser, User
 from django.test import TestCase, RequestFactory
 
 from lego.views import add_set, search
@@ -7,13 +7,13 @@ from . import test_settings
 
 
 @test_settings
-class TestGetRequest(TestCase):
+class TestSearch(TestCase):
     fixtures = ["test_data"]
 
     def setUp(self):
         self.factory = RequestFactory()
 
-    def test_search(self):
+    def test_get_request_anonymous_user(self):
         request = self.factory.get(
             "/lego/search/", query_params={"q": "brick", "mode": "name"}
         )
@@ -25,11 +25,49 @@ class TestGetRequest(TestCase):
         self.assertIn("Search Results", response.text)
         self.assertIn("Brick 2 x 4", response.text)
 
-    def test_add_set_redirects_to_login_if_not_authenticated(self):
+
+@test_settings
+class TestAddSet(TestCase):
+    fixtures = ["test_data", "test_user"]
+
+    def setUp(self):
+        self.factory = RequestFactory()
+
+    def test_get_request(self):
+        request = self.factory.get("/lego/set/add/")
+        request.user = User.objects.get()
+
+        response = add_set(request)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertIn("Lego Set ID:", response.text)
+
+    def test_get_request_anonymous_user(self):
         request = self.factory.get("/lego/set/add/")
         request.user = AnonymousUser()
 
         response = add_set(request)
 
+        # redirect to login
         self.assertEqual(response.status_code, 302)
-        self.assertStartsWith(response.url, "/lego/login")
+        self.assertIn("/login", response.url)
+
+    def test_post_request(self):
+        request = self.factory.post("/lego/set/add/", data={"set_lego_id": "123-1"})
+        request.user = User.objects.get()
+
+        response = add_set(request)
+
+        # set already exists, redirect to itself
+        self.assertEqual(response.status_code, 302)
+        self.assertIn("/set/add", response.url)
+
+    def test_post_request_anonymous_user(self):
+        request = self.factory.post("/lego/set/add/", data={"set_lego_id": "123-1"})
+        request.user = AnonymousUser()
+
+        response = add_set(request)
+
+        # redirect to login
+        self.assertEqual(response.status_code, 302)
+        self.assertIn("/login", response.url)

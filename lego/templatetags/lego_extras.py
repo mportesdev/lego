93751a332738769e453887e9fdeae7499376415e
Michal Porteš <michalportes1@gmail.com>
2026-03-20 23:00:18 +0100

    templates: Test file existence using the storage system

--- lego/templatetags/lego_extras.py
+++ lego/templatetags/lego_extras.py
@@ -1,9 +1,9 @@
 from django import template
-from django.conf import settings
+from django.core.files.storage import storages
 
 register = template.Library()
 
 
 @register.filter
 def is_static_file(rel_path):
-    return rel_path is not None and (settings.STATIC_ROOT / rel_path).is_file()
+    return rel_path is not None and storages["staticfiles"].exists(rel_path)

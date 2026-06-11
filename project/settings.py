5d173c630247b2eaf4caa7c037fbe9b548d45a22
Michal Porteš <michalportes1@gmail.com>
2026-06-11 23:15:12 +0200

    Access media files from templates

--- project/settings.py
+++ project/settings.py
@@ -53,6 +53,7 @@ TEMPLATES = [
             "context_processors": [
                 "django.template.context_processors.debug",
                 "django.template.context_processors.request",
+                "django.template.context_processors.media",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
                 "lego.context_processors.common_context",

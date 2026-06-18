2d41ea51739fb4dc273bf4cce87517e1a1650f9b
Michal Porteš <michalportes1@gmail.com>
2026-06-18 10:20:37 +0200

    Clean up templates (#169)
    * Remove the `is_static_file` template filter
    
    * templates: Simplify `if-elif` for image URLs

--- lego/templatetags/lego_extras.py
+++ lego/templatetags/lego_extras.py
@@ -1,9 +1,3 @@
 from django import template
-from django.core.files.storage import storages
 
 register = template.Library()
-
-
-@register.filter
-def is_static_file(rel_path):
-    return rel_path is not None and storages["default"].exists(rel_path)
